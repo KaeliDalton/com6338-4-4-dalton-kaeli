df6ace1a4852fc0acfe4825b27f130f14a2f5a64
Kaeli Dalton <Kaelidalton@ufl.edu>
2023-02-06 13:53:06 -0500

    winning half of answer recognition

--- index.js
+++ index.js
@@ -39,3 +39,46 @@ for (i=0; i < answer.length; i++){
 var board = wordPlayed.join("")
 console.log(answer)
 wordToGuessEl.textContent = board
+document.body.onkeyup = function(e){
+  var key = e.key.toLowerCase()
+  if (invalid.includes(key) == false && valid.includes(key) == false)
+  {
+  for(i=0; i < answer.length; i++){
+    if (answer[i] == key){
+      correct = true
+      lastLetters--
+      valid.push(key)
+      wordPlayed[i] = key
+
+    }else{
+    }
+  }
+  if(correct == true){
+    board = wordPlayed.join("")
+    wordToGuessEl.textContent = board
+    if(lastLetters == 0){
+      wins++
+      winsEl.textContent = wins
+      previousWordEl.textContent = answer
+      wordToGuessEl.textContent = ""
+      answer = words[Math.floor(Math.random() * words.length)]
+      wordPlayed = answer.split('')
+      for(i=0; i < answer.length; i++){
+        wordToGuessEl.textContent = wordToGuessEl.textContent + "_"
+        wordPlayed[i]='_'
+      }
+      guesses = 10
+      remainingGuessesEl.textContent = guesses
+      board = wordPlayed.join("")
+      console.log(answer)
+      wordToGuessEl.textContent = board
+      invalid = []
+      valid = []
+      lastLetters = answer.length
+      incorrectLetters.textContent = ""
+      }
+    }else{
+      
+    }
+}
+}
