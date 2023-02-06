eaae959c2bdbc852e8ae9277a5e15817e45a1746
Kaeli Dalton <Kaelidalton@ufl.edu>
2023-02-06 13:46:38 -0500

    variable definition

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ var words = [
   'ukulele',
   'mango'
 ]
+
+var wordToGuessEl = document.getElementById('word-to-guess')
+var remainingGuessesEl = document.getElementById('remaining-guesses')
+var winsEl = document.getElementById('wins')
+var lossesEl = document.getElementById('losses')
+var previousWordEl = document.getElementById('previous-word')
+var answer = words[Math.floor(Math.random() * words.length)]
+var incorrectLetters = document.getElementById('incorrect-letters')
+var guesses = 10
+var wins = 0
+var losses = 0
+var lastLetters = answer.length
+var correct = false
+var valid = []
+var invalid = []
